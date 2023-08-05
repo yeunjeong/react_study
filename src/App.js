21f456e9a61565f5a5145546aec305c92c97f37f
yeunjeong <araruu2536@gmail.com>
2023-08-05 21:59:40 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+
 function App() {
   return (
     <div>
       <h1>Hello!!</h1>
+      <Button text={"Button"} />
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,9 @@
+function Button({ text }) {
+  return (
+    <div>
+      <Button>{text}</Button>
+    </div>
+  );
+}
+
+export default Button;
